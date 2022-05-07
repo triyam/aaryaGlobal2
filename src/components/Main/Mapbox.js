104b57f3ad10fea582fc967a0cd93d579c940ffb
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-05-07 20:55:00 +0530

    mapbox made functional

--- src/components/Main/Mapbox.js
+++ src/components/Main/Mapbox.js
@@ -2,42 +2,59 @@ import React, { useRef, useEffect, useState } from 'react';
 
 // eslint-disable-next-line import/no-unresolved
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import jpMap from '../../assets/img/main/jpmap.png'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyeWFnbG9iYWwiLCJhIjoiY2wybXNydW5rMHZ0dDNicWN6OXk0OW9iYSJ9.sLWQvg-i51poM7WO4eOlqw';
 
-export default function App() {
+export default function Mapbox() {
   const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(138.2529);
-  const [lat, setLat] = useState(36.2048);
-  const [zoom, setZoom] = useState(5.5);
+  const [markerLngLat, setMarkerLngLat] = useState([138.2529, 36.2048]);
+  const [mapObject, setMap] = useState();
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      // eslint-disable-next-line object-shorthand
-      zoom: zoom
-    });
+useEffect(() => {
+  const map = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: markerLngLat,
+    zoom: 7,
   });
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
+  setMap(map);
+},[]);
+
+function setMapCenter(coords) {
+  if (mapObject) {
+    mapObject.setCenter(coords);
+  }
+}
 
   return (
+    <>
+
     <div>
       {/* <div className="sidebar">
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div> */}
       <div ref={mapContainer} className="map-container" />
     </div>
+    <div className="map_and_form ">
+      
+      <div className="map_search_form">
+         <img src={jpMap} alt=""/>
+
+         <a id="area_0" href="#/" onClick={() => setMapCenter([142.8635, 43.2203])}>HOKKAIDO</a>
+         <a id="area_1" href="#/" onClick={() => setMapCenter([141.1500, 39.1362])}>TOHOKU</a>
+         <a id="area_2" href="#/" onClick={() => setMapCenter([136.8115, 34.8586])}>CHUBU</a>
+         <a id="area_3" href="#/" onClick={() => setMapCenter([139.6911, 36.4599])}>KANTO</a>
+         <a id="area_4" href="#/" onClick={() => setMapCenter([133.6947, 35.0806])}>CHUGOKU</a>
+         <a id="area_5" href="#/" onClick={() => setMapCenter([135.5629, 34.6413])}>KANSAI</a>
+         <a id="area_6" href="#/" onClick={() => setMapCenter([134.5550, 34.0704])}>CHIGOKU</a>
+         <a id="area_7" href="#/" onClick={() => setMapCenter([130.8000, 32.5900])}>KYUSYU</a>
+         <a id="area_8" href="#/" onClick={() => setMapCenter([127.8056, 26.3344])}>OKINAWA</a>
+
+      </div>          
+    </div>
+   
+    </>
   );
 }
