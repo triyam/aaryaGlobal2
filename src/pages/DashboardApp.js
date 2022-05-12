91a2181ae4f3c0d6d6edeec1d72d4c362281bfd7
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-05-12 10:38:19 +0530

    onclick added in dashboard cards

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,5 +1,8 @@
+import React from 'react';
 // @mui
 import { Grid, Container, Typography } from '@mui/material';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 // components
 import Page from '../dashboardComponents/Page';
 
@@ -10,10 +13,29 @@ import {
 
 } from '../sections/@dashboard/app';
 
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 // ----------------------------------------------------------------------
 
 
 export default function DashboardApp() {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = () => setOpen(false);
+  const [text, setText] = React.useState('');
 
   return (
     <Page title="Dashboard">
@@ -24,15 +46,15 @@ export default function DashboardApp() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary style={{cursor:"pointer"}} title="End Users Registered" color="info" total={714000} icon={'fa-solid:users'} />
+            <AppWidgetSummary style={{cursor:"pointer"}} onClick={()=>{setText("End Users Registered"); handleOpen()}} title="End Users Registered" color="info" total={714000} icon={'fa-solid:users'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary style={{cursor:"pointer"}} title="Golf Owner Registered" total={1352831} color="info" icon={'maki:golf'} />
+            <AppWidgetSummary style={{cursor:"pointer"}} onClick={()=>{setText("Golf Owner Registered"); handleOpen()}} title="Golf Owner Registered" total={1352831} color="info" icon={'maki:golf'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary style={{cursor:"pointer"}} title="Travel Owner Registered" total={1723315} color="info" icon={'healthicons:travel-alt'} />
+            <AppWidgetSummary style={{cursor:"pointer"}} onClick={()=>{setText("Travel Owner Registered"); handleOpen()}} title="Travel Owner Registered" total={1723315} color="info" icon={'healthicons:travel-alt'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
@@ -74,6 +96,22 @@ export default function DashboardApp() {
           <Grid item xs={12} sm={6} md={3}>
             <AppWidgetSummary style={{cursor:"pointer"}} title="Revenue Through Customer Made Forms" total={234} color="info" icon={'emojione-monotone:money-bag'} />
           </Grid>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {text}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae!
+            </Typography>
+          </Box>
+          </Modal>
 
         </Grid>
       </Container>
