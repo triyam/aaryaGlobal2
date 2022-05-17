c5c7d98a3fd1a046734b84b1c87421e5c9e79fc8
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-05-17 17:25:06 +0530

    modal added in map pin click

--- src/components/Main/MapModal.js
+++ src/components/Main/MapModal.js
@@ -1,6 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
-
-// modal imports
+import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -15,46 +13,8 @@ import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Link from '@mui/material/Link';
-// end
-
-// eslint-disable-next-line import/no-unresolved
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import jpMap from '../../assets/img/main/jpmap.png'
 
 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyeWFnbG9iYWwiLCJhIjoiY2wybXNydW5rMHZ0dDNicWN6OXk0OW9iYSJ9.sLWQvg-i51poM7WO4eOlqw';
-
-
-const geojson = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [142.8235, 43.6203]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'Hokaido 1, Japan'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [142.8535, 43.1203]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'Hokaido 2, Japan'
-      }
-    }
-  ]
-};
-
-// modal functions
 const style = {
   position: "absolute",
   top: "50%",
@@ -98,79 +58,31 @@ function a11yProps(index) {
     id: `simple-tab-${index}`,
     "aria-controls": `simple-tabpanel-${index}`
   };
-} 
-// end
-export default function Mapbox() {
-// modal handler
-const [open, setOpen] = React.useState(false);
-const handleOpen = () => setOpen(true);
-const handleClose = () => setOpen(false);
-
-const [value, setValue] = React.useState(0);
-
-const handleChange = (event, newValue) => {
-  setValue(newValue);
-};
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const data = new FormData(event.currentTarget);
-  console.log({
-    email: data.get("email"),
-    password: data.get("password")
-  });
-};
-// ends
-
-  const mapContainer = useRef(null);
-  const [markerLngLat, setMarkerLngLat] = useState([138.2529, 36.2048]);
-  const [mapObject, setMap] = useState();
-
-
-useEffect(() => {
-  const map = new mapboxgl.Map({
-    container: mapContainer.current,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: markerLngLat,
-    zoom: 7,
-  });
-
-  setMap(map);
-  // add markers to map
-  // eslint-disable-next-line no-restricted-syntax
-  for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
-
-  new mapboxgl.Marker(el)
-  .setLngLat(feature.geometry.coordinates)
-  .addTo(map);
-  el.addEventListener('click', () => 
-  { 
-     // eslint-disable-next-line no-alert
-    //  alert("Marker Clicked.");
-    handleOpen();
-  }
-)
-
 }
-},[]);
 
-function setMapCenter(coords) {
-  if (mapObject) {
-    mapObject.setCenter(coords);
-  }
-}
+export default function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password")
+    });
+  };
 
   return (
-    <>
-
-<div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -341,28 +253,5 @@ function setMapCenter(coords) {
         </Box>
       </Modal>
     </div>
-
-    <div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
-    <div className="map_and_form ">
-
-      <div className="map_search_form">
-         <img src={jpMap} alt=""/>
-
-         <a id="area_0" href="#/" onClick={() => setMapCenter([142.8635, 43.2203])}>HOKKAIDO</a>
-         <a id="area_1" href="#/" onClick={() => setMapCenter([141.1500, 39.1362])}>TOHOKU</a>
-         <a id="area_2" href="#/" onClick={() => setMapCenter([136.8115, 34.8586])}>CHUBU</a>
-         <a id="area_3" href="#/" onClick={() => setMapCenter([139.6911, 36.4599])}>KANTO</a>
-         <a id="area_4" href="#/" onClick={() => setMapCenter([133.6947, 35.0806])}>CHUGOKU</a>
-         <a id="area_5" href="#/" onClick={() => setMapCenter([135.5629, 34.6413])}>KANSAI</a>
-         <a id="area_6" href="#/" onClick={() => setMapCenter([134.5550, 34.0704])}>CHIGOKU</a>
-         <a id="area_7" href="#/" onClick={() => setMapCenter([130.8000, 32.5900])}>KYUSYU</a>
-         <a id="area_8" href="#/" onClick={() => setMapCenter([127.8056, 26.3344])}>OKINAWA</a>
-
-      </div>          
-    </div>
-   
-    </>
   );
 }
